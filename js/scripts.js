754835f087312497d88bc0d6091995ca73c807bc
Pamela Ormeño <pamelacovarrubias@gmail.com>
2025-02-22 19:23:16 -0300

    Proyecto base

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.text-gradient').forEach(link => {
+        link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noopener noreferrer');
+    });
+});
