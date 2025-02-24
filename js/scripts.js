ae9a9bd28fd556d6c6cf7d14bf23ea4abd74155a
Pamela Ormeño <pamelacovarrubias@gmail.com>
2025-02-24 20:06:53 -0300

    Implementación de la sección recomendaciones

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,19 @@ scrollToTopButton.addEventListener('click', () => {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+
+    document.getElementById("next").addEventListener("click", function () {
+        slides[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % slides.length;
+        slides[currentIndex].classList.add("active");
+    });
+
+    document.getElementById("prev").addEventListener("click", function () {
+        slides[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        slides[currentIndex].classList.add("active");
+    });
+});
